c10e1dc2e3a5062304264d3a096d1cc612f19f39
Samuel Torres <rilla@MacBook-Pro.local>
2026-01-28 19:25:05 -0500

    Creating projects with pip and installing dependencies

---
+++ uv-setup/main.py
@@ -0,0 +1,12 @@
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+#     "requests",
+#     "rich>=14.3.1",
+# ]
+# ///
+
+import sys
+from requests import get
+
+print("Python version:", sys.version)
